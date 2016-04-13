c21bf7db592bff0ba55c3dfa8d3514f28495e1b7
igortat <itat.pub@gmail.com>
2016-04-13 09:37:51 +0300

    us#0: spec integration test: drop test db, POST, GET, check min/max/avg

--- spec/spec00.rb
+++ spec/spec00.rb
@@ -1,6 +1,7 @@
 #spec00.rb
 ENV['RACK_ENV'] = 'test'
-#ENV['MONGO_DATABASE'] = 'dbtest'  
+ENV['MONGO_HOSTADDR'] = '127.0.0.1:27017'  
+ENV['MONGO_DATABASE'] = 'dbtest'
 
 require 'rspec'
 require 'rack/test'
@@ -13,12 +14,28 @@ class RestApiTest < Test::Unit::TestCase
     RestApi
   end
 
-  def test_get_service
-    params = {'range' => [1460205628,1460205706]}
-    get '/2', :json => params.to_json
-#      print last_request.url
-#      print last_response.body    
-    assert last_response.body.include?("avg")
+  def test_integration
+    Mongo::Client.new([ENV['MONGO_HOSTADDR']], :database => ENV['MONGO_DATABASE']).database.drop
+    
+    payload = {
+      "1460205601" => 2,
+      "1460205648" => 4,
+      "1460205701" => 6
+    }
+    post '/42', payload.to_json, {'Content-Type' => 'application/json'} 
+    assert last_response.created?
+ 
+    params1 = {'range' => [1460205600,1460205700]}
+    get '/42', :json => params1.to_json
+    assert_equal JSON.parse(last_response.body)['avg'].to_i, 3
+    assert_equal JSON.parse(last_response.body)['max'].to_i, 4  
+        
+    params2 = {'range' => [1460205600,1460205800]}
+    get '/42', :json => params2.to_json
+    assert_equal JSON.parse(last_response.body)['min'].to_i, 2
+    assert_equal JSON.parse(last_response.body)['avg'].to_i, 4
+    assert_equal JSON.parse(last_response.body)['max'].to_i, 6  
+
   end
 
 end
